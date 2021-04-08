c13d1c9d278eeab25411f57e3c9734f82f509902
Sarayu2129 <trainer211986@gmail.com>
2021-04-07 23:48:58 -0400

    added the comment

--- src/main/java/com/actitime/base/DriverScript.java
+++ src/main/java/com/actitime/base/DriverScript.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 public class DriverScript {
 	
 	public static WebDriver driver;
-	Properties  prop;
+	Properties  prop;// this is the propertie file
 
 	public DriverScript(){
 		try{
